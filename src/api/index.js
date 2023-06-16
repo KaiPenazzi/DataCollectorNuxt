53de4f325d89044e06ca51c3b5367b4c6632cee0
KaiPenazzi <k.penazzi@web.de>
2023-06-16 17:35:45 +0200

    api methods

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import { PrismaClient } from '@prisma/client'
+import { applyPlugin } from 'nuxt/app'
 
 const prisma = new PrismaClient()
 const app = express()
@@ -13,3 +14,46 @@ export default {
   path: '/api',
   handler: app
 }
+
+app.post('/user', async (req, res) => {
+    const result = await prisma.user.create({
+        data: {
+            email: req.body.email,
+            password: req.body.password
+        }
+    })
+    res.json(result)
+})
+
+app.post('/devices', async(req, res) => {
+    const { name, username, device_id, key, userEmail} = req.body
+    const device = await prisma.post.create({
+        data: {
+            name,
+            username,
+            device_id,
+            key,
+            connect: {
+                email: userEmail
+            }
+        }
+    })
+    res.status(200).json(device)
+})
+
+app.get('/devices', async(req, res) => {
+    const devices = await prisma.post.findMany({
+        
+    })
+    res.json(posts)
+})
+
+app.get('/devices/:id', async(req,res) => {
+    const {id} = req.params
+    const device = await prisma.post.findUnique({
+        where:  {
+            id: Number(id)
+        }
+    })
+    res.json(post)
+})
