5472140f4c070502a7640005de1e54715f6f24c8
KaiPenazzi <k.penazzi@web.de>
2023-06-17 20:18:33 +0200

    login and register in own folder

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="/home">Home</a>
+            <a class="navbar-brand" href="/Home">Home</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -16,10 +16,10 @@
                         <a class="nav-link" href="/actions">Actions</a>
                     </li>
                     <li class="nav-item active">
-                        <a class="nav-link" href="/login">login</a>
+                        <a class="nav-link" href="/user/login">login</a>
                     </li>
                     <li class="nav-item active">
-                        <a class="nav-link" href="/register">register</a>
+                        <a class="nav-link" href="/user/register">register</a>
                     </li>
                 </ul>
             </div>
