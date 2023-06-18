314d40ba6af3064d39d670d17626726d66dfe8a0
KaiPenazzi <k.penazzi@web.de>
2023-06-18 16:29:49 +0200

    add tool hexToBase64

---
+++ src/tools/hexToBase64.ts
@@ -0,0 +1,11 @@
+function hexToBase64(hexString:String) {
+    // Konvertiere den Hex-String in ein Buffer-Objekt
+    const buffer = Buffer.from(hexString, 'hex');
+
+    // Konvertiere den Buffer in einen Base64-String
+    const base64String = buffer.toString('base64');
+
+    return base64String;
+}
+
+module.exports = {hexToBase64}
