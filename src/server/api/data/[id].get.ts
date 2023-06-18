b65adc69172619b200c2c5060bb6487a759966cc
KaiPenazzi <k.penazzi@web.de>
2023-06-18 20:43:07 +0200

    when start the app, mqtt connects to all devices

--- src/server/api/data/[id].get.ts
+++ src/server/api/data/[id].get.ts
@@ -1,4 +1,4 @@
-import {prisma} from '../../../prisma/prisma'
+import prisma from '../../../prisma/prisma'
 import cookieParser from '../../../tools/cookieParser'
 
 export default defineEventHandler(async (event) => {
