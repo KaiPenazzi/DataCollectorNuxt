26e7313e54c6d7facecece9e2c35eb46848be416
KaiPenazzi <k.penazzi@web.de>
2023-06-19 17:58:41 +0200

    actions view

--- src/pages/Actions.vue
+++ src/pages/Actions.vue
@@ -1,3 +1,43 @@
+<script setup>
+
+definePageMeta({
+    middleware: [
+        'auth'
+    ]
+})
+
+const {data: actions} = useFetch('/api/actions')
+
+function openAdd() {
+    navigateTo('/action/add')
+}
+
+function edit(id) {
+    navigateTo('/action/' + id)
+}
+
+</script>
+
 <template>
-    
+    <button class="btn btn-success" @click="openAdd">add</button>
+    <table class="table">
+        <tbody>
+            <tr v-for="item in actions">
+                <td>
+                    <div class="card">
+                        <div class="card-header">{{ item.name }}</div>
+                        <div class="card-body">
+                            <div class="form-check form-switch"><input class="form-check-input" type="checkbox" :checked="item.state"/></div>
+                        </div>
+                    </div>
+                </td>
+                <td class="col-3">
+                    <button class="btn btn-danger" @click="remove(item.id)">remove</button>
+                    <button class="btn btn-primary" @click="edit(item.id)">edit</button>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+
+
 </template>
---
+++ src/server/api/actions.get.ts
@@ -0,0 +1,21 @@
+import prisma from '../../prisma/prisma'
+import cookieParser from '../../tools/cookieParser'
+
+export default defineEventHandler(async (event) => {
+    const cookies = cookieParser(event.node.req.headers.cookie)
+
+    const user = await prisma.user.findUnique({
+        where: {
+            email: cookies["email"],
+        },
+        include: {
+            actions: true,
+        },
+    });
+
+    if (!user) {
+        return []; // oder eine geeignete Fehlerbehandlung
+    }
+
+    return user.actions;
+})
