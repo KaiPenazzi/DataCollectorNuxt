0b14327a2eba3464281703fb4ba35e22a1d4c072
KaiPenazzi <k.penazzi@web.de>
2023-06-24 00:00:51 +0200

    fix home view

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -11,7 +11,7 @@ const { data } = await useFetch('/api/data/overview')
 const router = useRouter()
 
 setInterval(() => {
-    router.go(0)
+    //router.go(0)
 }, 10000)
 
 function formatDate(timestamp) {
@@ -29,19 +29,24 @@ function openData(id) {
     <table class="table">
         <tbody>
             <tr v-for="item in data.powerfox">
-                <div class="card">
-                    <div class="card-header"><a @click="openDataPF(item.powerfox.id)" style="text-decoration: underline;">{{
-                        item.powerfox.name }} | {{ formatDate(item.data.timestamp) }}</a></div>
-                    <div class="card-body">
-                        {{ item.data.watt }}
+                <td>
+                    <div class="card">
+                        <div class="card-header"><a @click="openDataPF(item.powerfox.id)"
+                                style="text-decoration: underline;">{{
+                                    item.powerfox.name }} | {{ formatDate(item.data.timestamp) }}</a>
+                        </div>
+                        <div class="card-body">
+                            {{ item.data.watt }}
+                        </div>
                     </div>
-                </div>
+                </td>
             </tr>
             <tr v-for="item in data.mqtt">
                 <td>
                     <div class="card">
                         <div class="card-header"><a @click="openData(item.device.id)" style="text-decoration: underline;">{{
-                            item.device.name }} | {{ formatDate(item.data.received_at) }}</a></div>
+                            item.device.name }} | {{ formatDate(item.data.received_at) }}</a>
+                        </div>
                         <div class="card-body">
                             <ul>
                                 <li v-for="(value, key) in item.data.uplink_message.decoded_payload" :key="key">
